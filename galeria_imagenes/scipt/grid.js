4650247fd258b6bf1df4e99f365aa66dd34407e9
Alicia <alisafedorova@alu.easda.es>
2024-11-14 08:37:00 +0100

    cambios galería

---
+++ galeria_imagenes/scipt/grid.js
@@ -0,0 +1,8 @@
+var elements = document.getElementsByClassName("column");
+var i;
+function three() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
